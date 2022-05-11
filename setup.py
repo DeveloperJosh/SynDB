8fd74ad054bba8a06ed4a1d16bc3bf4a833c9cbe
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-11 18:54:06 -0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="SynDB",
+    version="1.6",
+    description="simple database using json.",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="Josh Wells",
+    author_email="bam0909@outlook.com",
+    license="MIT",
+    url="https://github.com/Synterra-Technologies/SynDB",
+    py_modules=['syndb'],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Database',
+    ],
+)
